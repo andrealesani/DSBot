dfe303595701d2a303102c1f28633a82315ddabe
giubots <giulioantonio.abbo@gmail.com>
2021-09-03 11:33:58 +0200

    implement tuning_mixins tests

--- DSBot/tuning/tuning_mixins.py
+++ DSBot/tuning/tuning_mixins.py
@@ -1,23 +1,55 @@
-import json
+"""Add info related with tuning to ir classes.
 
+The tuning_kb must be as follows:
+{
+    "module_name": {
+        "name": "Readable module name",
+        "params": {
+            "param_name": {
+                "name": "Readable param name",
+                "desc": "A description of what this param does"
+            }
+        }
+    }
+}
+"""
+import json
 import logging
+from pathlib import Path
+from typing import Any
 
 # Load the tuning kb data from file.
-with open("tuning_kb.json", "r") as tuning_kb_file:
-    print("reading tuning kb file")  # TODO(giubots): remove
+with open(Path(__file__).parent / 'tuning_kb.json', 'r') as tuning_kb_file:
+    logging.getLogger(__name__).debug('Reading tuning_kb file')
     tuning_kb = json.loads(tuning_kb_file.read())
+
+
+class MissingModuleNameError(Exception):
+    """Raised when an IRPar does not have the module name set."""
+
+    def __init__(self, param_name):
+        super().__init__(f'This IRPar does not have the module name set: {param_name}')
+
+
+class IncorrectKbError(Exception):
+    """Raised when the structure of the tuning_kb is not correct."""
+    pass
 
 
 class TuningOpMixin:
     """Adds tuning functionalities to a IROp class."""
+    name: str
+    parameters: dict
+    _highlighted: bool
 
     @property
     def pretty_name(self):
         """Returns the printable name of this IROp, `self.name` if not found."""
 
-        if self.name in tuning_kb and "name" in tuning_kb[self.name]:
-            return tuning_kb[self.name]["name"]
-        logging.getLogger(__name__).warning("missing pretty name for module: %s", self.name)
+        try:
+            return tuning_kb[self.name]['name']
+        except KeyError:
+            logging.getLogger(__name__).warning('Missing pretty name for module: %s', self.name)
         return self.name
 
     @property
@@ -33,59 +65,80 @@ class TuningOpMixin:
 
     def to_json(self):
         return {
-            "name": self.name,
-            "pretty_name": self.pretty_name,
-            "parameters": {k: v.to_json() for k, v in self.parameters},
-            "is_highlighted": self.is_highlighted
+            'name': self.name,
+            'pretty_name': self.pretty_name,
+            'parameters': {k: v.to_json() for k, v in self.parameters},
+            'is_highlighted': self.is_highlighted
         }
 
 
 class TuningOpOptionsMixin:
     """Adds tuning functionalities to a IROpOptions class."""
+    actual_model: Any
+    models: dict
 
     def to_json(self):
         actual = self.actual_model.to_json()
-        actual["models"] = {k: v.to_json() for k, v in self.models}
+        actual['models'] = {k: v.to_json() for k, v in self.models}
         return actual
 
 
 class TuningParMixin:
-    """Adds tuning functionalities to a IRPar class."""
+    """Adds tuning functionalities to a IRPar class; attribute `module` must be set."""
+    name: str
+    module: str
+    value: Any
+    default_value: Any
+    min_v: Any
+    max_v: Any
+    _highlighted: bool
 
     @property
     def pretty_name(self):
         """Returns the printable name of this IRPar, `self.name` if not found."""
 
-        param = self._get_param_data()
-
-        if param is not None and "name" in param:
-            return param["name"]
-        logging.getLogger(__name__).warning("missing pretty name for param: %s", self.name)
+        try:
+            return self._get_param_data()['name']
+        except IncorrectKbError as e:
+            logging.getLogger(__name__).warning(e)
+        except KeyError:
+            logging.getLogger(__name__).warning('Missing pretty name for param: %s; module: %s', self.name, self.module)
         return self.name
 
     @property
     def description(self):
         """Returns the description of this IRPar, an empty string if not found."""
 
-        param = self._get_param_data()
-
-        if param is not None and "desc" in param:
-            return param["desc"]
-        logging.getLogger(__name__).warning("missing description for param: %s", self.name)
-        return ""
+        try:
+            return self._get_param_data()['desc']
+        except IncorrectKbError as e:
+            logging.getLogger(__name__).warning(e)
+        except KeyError:
+            logging.getLogger(__name__).warning('Missing description for param: %s; module: %s', self.name, self.module)
+        return ''
 
     def _get_param_data(self):
-        if self.module is None:
-            error = f"missing module name for parameter: {self.name}." \
-                    f"Check if its module is calling super.__init__(...)"
-            logging.getLogger(__name__).exception(error)
-            raise Exception(error)
+        """Returns the parameter data dictionary for this parameter.
 
-        if self.module in tuning_kb \
-                and "params" in tuning_kb[self.module] \
-                and self.name in tuning_kb[self.module]["params"]:
-            return tuning_kb[self.module]["params"][self.name]
-        return None
+        :raise MissingModuleNameError: if this IrPar does not have the module name set
+        :raise IncorrectKbError: if the structure of the tuning_kb is not correct
+        """
+        try:
+            if self.module is None:
+                raise AttributeError()
+
+            if self.module in tuning_kb:
+                if 'params' in tuning_kb[self.module]:
+                    if self.name in tuning_kb[self.module]['params']:
+                        return tuning_kb[self.module]['params'][self.name]
+                    raise IncorrectKbError(f'Missing param data for param: {self.name}; module: {self.module}')
+                raise IncorrectKbError(f'Missing params for module: {self.module}')
+            raise IncorrectKbError(f'Missing module: {self.module}')
+
+        except AttributeError:
+            error = MissingModuleNameError(self.name)
+            logging.getLogger(__name__).error(error)
+            raise error
 
     @property
     def is_highlighted(self):
@@ -100,12 +153,12 @@ class TuningParMixin:
 
     def to_json(self):
         return {
-            "name": self.name,
-            "pretty_name": self.pretty_name,
-            "value": self.value,
-            "min": self.min_v,
-            "max": self.max_v,
-            "default": self.default_value,
-            "description": self.description,
-            "is_highlighted": self.is_highlighted,
+            'name': self.name,
+            'pretty_name': self.pretty_name,
+            'value': self.value,
+            'min': self.min_v,
+            'max': self.max_v,
+            'default': self.default_value,
+            'description': self.description,
+            'is_highlighted': self.is_highlighted,
         }
---
+++ tests/tuning/test_tuning_mixins.py
@@ -0,0 +1,120 @@
+import json
+from pathlib import Path
+from unittest import TestCase
+
+import DSBot.tuning.tuning_mixins as mixins
+from DSBot.ir.ir_operations import IROp
+from DSBot.ir.ir_parameters import IRPar
+
+
+def change_file():
+    with open(Path(__file__).parent / 'test_tuning_kb.json', 'r') as tuning_kb_file:
+        mixins.tuning_kb = json.loads(tuning_kb_file.read())
+
+
+class TestTuningOpMixin(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+        change_file()
+
+    def test_pretty_name(self):
+        mod = IROp('mod1', {})
+        self.assertEqual(mod.pretty_name, 'Module 1')
+
+    def test_pretty_name_missing(self):
+        mod = IROp('mod3', {})
+        self.assertEqual(mod.pretty_name, 'mod3')
+
+    def test_is_highlighted(self):
+        mod = IROp('mod', {})
+        mod.is_highlighted = True
+        self.assertTrue(mod.is_highlighted)
+        mod.is_highlighted = False
+        self.assertFalse(mod.is_highlighted)
+        mod.is_highlighted = True
+        self.assertTrue(mod.is_highlighted)
+
+    def test_is_highlighted_not_set(self):
+        mod = IROp('mod', {})
+        self.assertFalse(mod.is_highlighted)
+
+
+# noinspection PyStatementEffect
+class TestTuningParMixin(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+        change_file()
+
+    def test_get_param_data_missing_module(self):
+        par = IRPar('param', 0, 0, 1)
+        with self.assertRaises(mixins.MissingModuleNameError):
+            par._get_param_data()
+
+    def test_get_param_data_none_module(self):
+        par = IRPar('param', 0, 0, 1)
+        par.module = None
+        with self.assertRaises(mixins.MissingModuleNameError):
+            par._get_param_data()
+
+    def test_get_param_data_missing_module_data(self):
+        par = IRPar('param', 0, 0, 1)
+        par.module = "missing"
+        with self.assertRaises(mixins.IncorrectKbError):
+            par._get_param_data()
+
+    def test_get_param_data_missing_params_data(self):
+        par = IRPar('param', 0, 0, 1)
+        par.module = "mod2"
+        with self.assertRaises(mixins.IncorrectKbError):
+            par._get_param_data()
+
+    def test_get_param_data_missing_data(self):
+        par = IRPar('param', 0, 0, 1)
+        par.module = "mod1"
+        with self.assertRaises(mixins.IncorrectKbError):
+            par._get_param_data()
+
+    def test_pretty_name(self):
+        par = IRPar('param1.1', 0, 0, 1)
+        par.module = 'mod1'
+        self.assertEqual(par.pretty_name, 'Param 1.1')
+
+    def test_pretty_name_missing(self):
+        par = IRPar('param1.2', 0, 0, 1)
+        par.module = 'mod1'
+        self.assertEqual(par.pretty_name, 'param1.2')
+
+    def test_pretty_name_missing_mod(self):
+        par = IRPar('param1.1', 0, 0, 1)
+        with self.assertRaises(mixins.MissingModuleNameError):
+            par.pretty_name
+
+    def test_description(self):
+        par = IRPar('param1.1', 0, 0, 1)
+        par.module = 'mod1'
+        self.assertEqual(par.description, 'Parameter 1 of module 1')
+
+    def test_description_missing(self):
+        par = IRPar('param1.2', 0, 0, 1)
+        par.module = 'mod1'
+        self.assertEqual(par.description, '')
+
+    def test_description_missing_mod(self):
+        par = IRPar('param1.1', 0, 0, 1)
+        with self.assertRaises(mixins.MissingModuleNameError):
+            par.description
+
+    def test_is_highlighted(self):
+        par = IRPar('param3', 0, 0, 1)
+        par.is_highlighted = True
+        self.assertTrue(par.is_highlighted)
+        par.is_highlighted = False
+        self.assertFalse(par.is_highlighted)
+        par.is_highlighted = True
+        self.assertTrue(par.is_highlighted)
+
+    def test_is_highlighted_not_set(self):
+        par = IRPar('param3', 0, 0, 1)
+        self.assertFalse(par.is_highlighted)
