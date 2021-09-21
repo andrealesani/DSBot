e7b7acfba6eaf0829d0fd22f5aff5c289c1369a0
giubots <giulioantonio.abbo@gmail.com>
2021-09-21 15:53:22 +0200

    fix: add check on range

--- DSBot/ir/ir_parameters.py
+++ DSBot/ir/ir_parameters.py
@@ -15,6 +15,7 @@ class IRPar(TuningParMixin, object):
     """
 
     def __init__(self, name: str, value: float, v_type: str, min_v: float, max_v: float, step: float):
+        assert min_v <= value <= max_v, 'The value must be inside the min_v max_v range'
         self.name = name
         self.default_value = value
         self.v_type = v_type
